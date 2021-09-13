6efa5dc53950bd1334e4f12dd972913764776ee9
Gorbovskyfallz <funkydrumma@yandex.ru>
2021-09-13 21:52:38 +0500

    testing russian language in git

--- something shitty.md
+++ something shitty.md
@@ -1,1 +1,1 @@
-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  -- for pushing
+	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  -- for pushing
--- stolyarov_lessons/.ipynb_checkpoints/hello5-checkpoint.asm
+++ stolyarov_lessons/.ipynb_checkpoints/hello5-checkpoint.asm
@@ -1,11 +1,11 @@
-    %include "stud_io.inc" 
-    global _start
+%include    "stud_io.inc"        ; including file-library with macro 
+global      _start               ; enter point of program for operating system
 
-    section .text
-    _start: mov     eax, 0
-    again:  PRINT   "Hello"
-            PUTCHAR 10
-            inc     eax
-            cmp     eax, 5
-            jl      again
+section     .text                
+_start:     mov         eax, 0
+again:      PRINT       "Hello"
+            PUTCHAR     10
+            inc         eax
+            cmp         eax, 5
+            jl          again
             FINISH
